0d8ec5ef09cf2f88043e1544f9b46b53423a1818
prestondunton <preston.dunton@gmail.com>
2020-12-18 13:52:46 -0700

    added more model / results persistence

--- front_end/front-end.py
+++ front_end/front-end.py
@@ -62,8 +62,10 @@ class Application():
         probabilities.columns = prediction_array
         probabilities.columns = [*probabilities.columns[:-1], 'probability']
         probabilities.insert(0, "character", self.model.classes_)
+        probabilities.sort_values(by=['probability'], ascending=False, inplace=True)
 
-        st.dataframe(probabilities.style.apply(self.custom_style, axis=1))
+        #st.dataframe(probabilities.style.apply(self.custom_style, axis=1))
+        st.dataframe(probabilities)
 
     def custom_style(self, row):
         if row.values[0] == self.prediction:
@@ -75,10 +77,40 @@ class Application():
 
 
     def render_app(self):
+        st.set_page_config(page_title='Marvel Dialogue Classification', layout='centered', \
+                           initial_sidebar_state='auto', page_icon="./images/marvel-favicon.png")
+
         self.render_header()
         self.render_input_box()
         self.render_prediction()
         self.render_probability_table()
+
+        if st.button('Say hello'):
+            st.write('Why hello there')
+        else:
+            st.write('Goodbye')
+
+        agree = st.checkbox('I agree')
+        if agree:
+            st.write('Great!')
+
+        genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'))
+        if genre == 'Comedy':
+            st.write('You selected comedy.')
+        else:
+            st.write("You didn't select comedy.")
+
+        option = st.selectbox('How would you like to be contacted?',('Email', 'Home phone', 'Mobile phone'))
+        st.write('You selected:', option)
+
+        options = st.multiselect('What are your favorite colors',['Green', 'Yellow', 'Red', 'Blue'],['Yellow', 'Red'])
+        st.write('You selected:', options)
+
+        values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
+        st.write('Values:', values)
+
+        color = st.select_slider('Select a color of the rainbow', options = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+        st.write('My favorite color is', color)
 
 
 app = Application()
